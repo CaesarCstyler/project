c3a2815eaa2a5408d5e1bb1c31c6e010bc1d6bfb
CaesarIman <bananad196@gmail.com>
2023-02-26 14:42:53 +0600

    login and signup logics

--- sneakers_vue/src/router/index.js
+++ sneakers_vue/src/router/index.js
@@ -4,6 +4,9 @@ import Home from '../views/HomeView.vue'
 import Sneakers from '../views/SneakersView.vue'
 import Category from '../views/CategoryView.vue'
 import Search from '../views/SearchView.vue'
+import Cart from '../views/CartView.vue'
+import SignUp from '../views/SignUpView.vue'
+import LogIn from '../views/LogInView.vue'
 
 const routes = [
   {
@@ -20,9 +23,24 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
   {
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/log-in',
+    name: 'LogIn',
+    component: LogIn
+  },
+  {
     path: '/search',
     name: 'Search',
     component: Search
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
   },
   {
     path: '/:category_slug/:sneakers_slug',
